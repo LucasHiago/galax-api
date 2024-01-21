410214ebeec42d3c6cdccbbc2ecf10f4edc4d38f
LucasHDSF <lucashiago.dsf@gmail.com>
2024-01-20 23:38:38 -0300

    Users CREATED
    READ
    CREATE
    UPDATE
    DELETE

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @ApiProperty({ required: false })
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+}
