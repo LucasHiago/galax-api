285611194606609b557efbfb7b1a2ef16932d602
LucasHDSF <lucashiago.dsf@gmail.com>
2024-01-24 19:06:09 -0300

    Both Endpoint
    Readme update

--- src/bothProducts/both.module.ts
+++ src/bothProducts/both.module.ts
@@ -4,9 +4,11 @@ import { BothService } from './service/both.service';
 import { ProductModule } from '../products/product.module';
 import { ServicesModule } from '../services/services.module';
 import { ComboItemModule } from '../comboItem/comboitem.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BothItems } from './entities/both.entity';
 
 @Module({
-  imports: [ProductModule, ServicesModule, ComboItemModule],
+  imports: [TypeOrmModule.forFeature([BothItems]), ProductModule, ServicesModule, ComboItemModule],
   controllers: [BothController],
   providers: [BothService],
 })
